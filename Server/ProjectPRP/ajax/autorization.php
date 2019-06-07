3258bc6e1ca067f1bce2843c4e3c9b1b8b25830e
Bohdan <44240067+nikbod1k@users.noreply.github.com>
2019-06-07 16:12:05 +0300

    Server1

---
+++ Server/ProjectPRP/ajax/autorization.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$email = $_POST['login'];
+$pass = md5($_POST['password']);
+
+    $mysql = new mysqli("localhost", "root", "", "users");
+    if ($mysql->connect_errno) {
+        printf("Не удалось подключиться: %s\n", $mysql->connect_error);
+        exit();
+    }
+    $mysql->query("SET NAMES 'utf-8");
+    $result = $mysql->query("SELECT * FROM users");
+
+    $flag=false;
+    foreach ($result as $user){
+    if($email==$user['Email']&&$pass==$user['Password']){
+        $_SESSION["loggedUser"] = $user;
+        $flag=true;
+        break;
+
+    }
+    }
+if($flag==true){
+    echo "http://localhost/projectPRP/account.php";
+
+}else {
+    echo "fail";
+}
+
+?>
---
+++ Server/ProjectPRP/registration.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AbitPro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/signin.css">
+
+</head>
+<body>
+<form action="index.php" method="post" class="form-signin">
+    <h1 class="h3 mb-3  font-weight-normal">Regestration:</h1>
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" name="pass" id="inputPassword" class="form-control" placeholder="Password" required="">
+
+    <label for="inputName" class="sr-only">Name</label>
+    <input type="text" name="name" id="inputName" class="form-control" placeholder="Name" required="">
+
+    <label for="inputSurname" class="sr-only">Surname</label>
+    <input type="text" name="surname" id="inputsurname" class="form-control" placeholder="Surname" required="">
+
+    <label for="inputThirdName" class="sr-only">ThirdName</label>
+    <input type="text" name="ThirdName" id="inputThirdName" class="form-control" placeholder="ThirdName" required="">
+
+
+    <p><strongChoose Subject</strong></p>
+    <p><select id="selectSubj1"class="selectSubj" name="hero">
+            <option value="Украинский" selected>Украинский</option>
+            <option value="Математика" >Математика</option>
+            <option value="Английский">Английский</option>
+            <option value="Физика">Физика</option>
+        </select>
+
+
+    <p><strongChoose Subject</strong></p>
+    <p><select id="selectSubj2"class="selectSubj" name="hero">
+            <option value="Украинский" selected>Украинский</option>
+            <option value="Математика" >Математика</option>
+            <option value="Английский">Английский</option>
+            <option value="Физика">Физика</option>
+        </select>
+
+    <p><strongChoose Subject</strong></p>
+    <p><select id="selectSubj3" class="selectSubj" name="hero">
+            <option value="Украинский" selected>Украинский</option>
+            <option value="Математика" >Математика</option>
+            <option value="Английский">Английский</option>
+            <option value="Физика">Физика</option>
+        </select>
+
+
+
+        <button class="btn btn-lg btn-primary btn-block mt-5" id="sendBtn" name="send" type="button">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">© 2017-2019</p>
+</form>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="js/do.js"></script>
+</body>
+</html>
