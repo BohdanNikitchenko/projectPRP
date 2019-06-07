4a4fb8df3b2657c4d598366f7fd7f50207923a11
Bohdan <Bohdan@DESKTOP-ESMFU6D>
2019-06-07 20:32:05 +0300

    Header

--- Server/ProjectPRP/account.php
+++ Server/ProjectPRP/account.php
@@ -5,7 +5,7 @@ $user=$_SESSION["loggedUser"];
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
@@ -19,15 +19,22 @@ $user=$_SESSION["loggedUser"];
     <script type="text/javascript" src="js/bootstrap.min.js.map"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/account.css">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/code.js"></script>
     <script type="text/javascript" src="js/acc.js"></script>
 
-
+    
     <title>Особистий кабінет</title>
     <link rel="shortcut icon" href="photo_2019-05-24_00-38-31.png" type="image/png">
 </head>
-<body link="black" vlink="black" alink="black">
+
+<body>
+<header>
+    <?php
+    require "header.php";
+    ?>
+</header>
 <a name="top"></a>
 <img id="imgacc" src="img/user-account-box.png">
 <a id="acc" href="Account.html">Особистий<br> кабінет</a>
---
+++ Server/ProjectPRP/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
+    <script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/bootstrap.bundle.js.map"></script>
+    <script type="text/javascript" src="js/bootstrap.js.map"></script>
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js.map"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js.map"></script>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">AbitPro</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse ml-5" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item  ml-5">
+                    <a class="nav-link" href="#">Новости </a>
+                </li>
+                <li class="nav-item dropdown ml-5 ">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ЗНО</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="#">Загальна информація</a>
+                        <a class="dropdown-item" href="#">Предмети ЗНО</a>
+                        <a class="dropdown-item" href="#">Тести</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ml-5" href="#">Спеціальності</a>
+                </li>
+                <li class="nav-item dropdown ml-5">
+                    <a class="nav-link dropdown-toggle " href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ЗВО</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="#">Перелік ЗВО</a>
+                        <a class="dropdown-item" href="#">Рейтинги</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Особистий кабінет</button>
+            </form>
+        </div>
+    </nav>
+
+
+    <script type="text/javascript" src="js/code.js"></script>
+
+
+
+    <title>Особистий кабінет</title>
+    <link rel="shortcut icon" href="photo_2019-05-24_00-38-31.png" type="image/png">
+</head>
+<body>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="js/do.js"></script>
+
+</body>
+</html>
