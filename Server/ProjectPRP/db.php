638d625ac69cb8761c0b9ec9687ef253960e1e7a
Pavel <pavlo.petrochenkov@nure.ua>
2019-06-10 11:26:26 +0300

    Merge branch 'Pavel'

---
+++ Server/ProjectPRP/db.php
@@ -0,0 +1,28 @@
+<?php
+
+$dbhost = "localhost";
+$dbname="users";
+$username="root";
+$password="";
+
+$db = new mysqli($dbhost,$username,$password,$dbname);
+if ($db->connect_errno) {
+    printf("Не удалось подключиться: %s\n", $mysql->connect_error);
+    exit();
+}
+$db->query("SET CHARACTER SET 'utf8'");
+
+function get_universities_all() {
+    global $db;
+    $universities = $db->query("SELECT * FROM universities");
+    return $universities;
+}
+
+
+function get_universities_by_id($id) {
+    global $db;
+    $universities = $db->query("SELECT * FROM universities WHERE id=$id");
+    foreach ($universities as $univer){
+    return $univer;
+}
+}
