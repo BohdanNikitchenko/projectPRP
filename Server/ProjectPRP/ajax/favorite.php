cff7fa59ad4a4e2d58d84e20416dfb85e073272f
Pavel <pavlo.petrochenkov@nure.ua>
2019-06-12 14:09:29 +0300

    last comit

--- Server/ProjectPRP/ajax/favorite.php
+++ Server/ProjectPRP/ajax/favorite.php
@@ -5,8 +5,9 @@ $dbname="users";
 $username="root";
 $password="";
 
-$favorite = intval($_POST['finger_val']);
-$id = intval($_POST['finger_id']);
+
+$id = intval($_POST['id']);
+$value = intval($_POST['value']);
 
 
 $mysql = new mysqli("localhost", "root", "", "users");
@@ -14,7 +15,13 @@ if ($mysql->connect_errno) {
     printf("Не удалось подключиться: %s\n", $mysql->connect_error);
     exit();
 }
-$mysql->query("SET NAMES 'utf-8");
-$result = $mysql->query("UPDATE universities SET favorite=1 WHERE id =$id");
+$mysql->query("SET NAMES 'utf-8'");
+
+if($value=='0'){
+    $result = $mysql->query("UPDATE universities SET favorite='1' WHERE id = '$id'");
+}
+if($value=='1') {
+    $result = $mysql->query("UPDATE universities SET favorite='0' WHERE id = '$id'");
+}
 
 ?>
